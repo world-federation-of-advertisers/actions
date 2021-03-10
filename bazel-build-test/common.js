6b13049c930b0e14ab8eea1a6bbe804f06e0605a
Sanjay Vasandani <sanjayvas@google.com>
2021-03-10 14:12:43 -0800

    Add restore-cache input param to bazel-build-test action. (#6)

--- bazel-build-test/common.js
+++ bazel-build-test/common.js
@@ -17,6 +17,11 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
+/** True values in YAML 1.2 Core Schema. */
+const YAML_TRUE_VALUES = Object.freeze(['true', 'True', 'TRUE']);
+/** False values in YAML 1.2 Core Schema. */
+const YAML_FALSE_VALUES = Object.freeze(['false', 'False', 'FALSE']);
+
 const workspacePath = core.getInput('workspace-path');
 
 async function execBash(commands) {
@@ -33,7 +38,27 @@ async function execBash(commands) {
   return lines.join('\n');
 }
 
+/**
+ * Returns the boolean value of the specified input.
+ *
+ * TODO(actions/toolkit#723): Remove this once there's an equivalent method in
+ * core.
+ */
+function getInputBool(name) {
+  const inputStr = core.getInput(name);
+  if (YAML_TRUE_VALUES.includes(inputStr)) {
+    return true;
+  }
+  if (YAML_FALSE_VALUES.includes(inputStr)) {
+    return false;
+  }
+
+  throw new TypeError(
+      `${inputStr} is not a valid YAML 1.2 Core Schema boolean`);
+}
+
 module.exports = {
   workspacePath: workspacePath,
   execBash: execBash,
+  getInputBool: getInputBool,
 };
