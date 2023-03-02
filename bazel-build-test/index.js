world-federation-of-advertisers/actions

commit 9babc7b424433413bd9f42596b152a373e7aeae4

Add excluded-cache-file-patterns option to bazel-build-test. (#31)
This excludes files from the build cache. By default it excludes tar archives.

The idea is to avoid filling the Actions cache with large files that can be cheaply regenerated.

bazel-build-test/index.js
@@ -32,7 +32,7 @@ async function run() {
   const execRootHash =
       await execBash([`echo -n '${execRoot}' | git hash-object --stdin`]);
   const treeHash = await execBash(['git rev-parse HEAD:']);
-  const outputBase = await execBash(['bazelisk info output_base'])
+  const outputBase = await execBash(['bazelisk info output_base']);
 
   const cacheVersion = core.getInput('cache-version');
   const cacheKey = `bazel-${cacheVersion}-${execRootHash}-${treeHash}`;
@@ -63,6 +63,11 @@ async function run() {
   const cacheSaveEvents = core.getMultilineInput('cache-save-events');
   if (targetPatterns.includes('//...') &&
       cacheSaveEvents.includes(github.context.eventName)) {
+    const excludedFileNamePatterns =
+        core.getMultilineInput('excluded-cache-file-patterns');
+    if (excludedFileNamePatterns.length > 0) {
+      await deleteExcludedFiles(excludedFileNamePatterns);
+    }
     try {
       await cache.saveCache(cachePaths, cacheKey);
     } catch (err) {
@@ -73,6 +78,22 @@ async function run() {
       }
     }
   }
+}
+
+async function deleteExcludedFiles(excludedFileNamePatterns) {
+  const findArgs = ['-type', 'f'];
+  for (let i = 0; i < excludedFileNamePatterns.length; i++) {
+    findArgs.push('-iname', excludedFileNamePatterns[i]);
+    if (i < excludedFileNamePatterns.length - 1) {
+      findArgs.push('-o');
+    }
+  }
+  findArgs.push('-delete');
+
+  const binDir = (await exec.getExecOutput('bazelisk', ['info', 'bazel-bin'], {
+                   cwd: workspacePath
+                 })).stdout.trimEnd();
+  await exec.exec('find', findArgs, {cwd: binDir});
 }
 
 (async function() {

.github/workflows/test-bazel-build-test.yml
@@ -55,6 +55,9 @@ jobs:
       uses: ./bazel-build-test
       with:
         workspace-path: .
+        cache-save-events: |
+          pull_request
+          push
   failing-test-target:
     name: Run with failing test target
     runs-on: ubuntu-20.04

bazel-build-test/action.yml
@@ -31,6 +31,16 @@ inputs:
       Newline-delimited.
     required: false
     default: push
+  excluded-cache-file-patterns:
+    # TODO(actions/toolkit#184): Use list type rather than newline-delimited
+    # string once it's available.
+    description: >-
+      File name patterns to exclude from the cache. Newline-delimited.
+      
+      These are passed to the `-iname` option of GNU find. The exclusion is
+      implemented by deleting the files prior to saving the cache.
+    required: false
+    default: '*.tar'
   build-options:
     # TODO(actions/toolkit#184): Use list type rather than newline-delimited
     # string once it's available.

commit 05c1e8ec0552981d90abaee1eebf265546b9e49e

Fix build options not being passed to `info bazel-bin` command. (#32)
Some build options, such as `--compilation_mode=opt`, can affect the
path.

Fixes a bug introduced in #31.

bazel-build-test/index.js
@@ -90,9 +90,11 @@ async function deleteExcludedFiles(excludedFileNamePatterns) {
   }
   findArgs.push('-delete');
 
-  const binDir = (await exec.getExecOutput('bazelisk', ['info', 'bazel-bin'], {
+  const bazelArgs = ['info'].concat(common.buildOptions).concat('bazel-bin');
+  const binDir = (await exec.getExecOutput('bazelisk', bazelArgs, {
                    cwd: workspacePath
                  })).stdout.trimEnd();
+
   await exec.exec('find', findArgs, {cwd: binDir});
 }
 